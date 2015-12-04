0c3dc1a9aa0638bcbf1c04f86a2eac19b1a293f7
Mario Baumgartner <mario.baumgartner92@gmx.at>
2015-12-04 10:35:26 +0100

    initial commit

---
+++ TheWalkingDead/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheWalkingDead
+//
+//  Created by Mario Baumgartner on 04.12.15.
+//  Copyright © 2015 Mario Baumgartner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
