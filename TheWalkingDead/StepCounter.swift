26c11e716def29fd11bb7f14d4da98f083137b0a
Thomas Kranzer <thomas.kranzer@gmx.at>
2015-12-04 12:16:46 +0100

    bla

--- TheWalkingDead/StepCounter.swift
+++ TheWalkingDead/StepCounter.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import CoreMotion
+
+class StepCounter {
+    
+    let manager = CMMotionManager()
+    var counter = 0
+    
+    init() {
+        startRecording()
+    }
+    
+    func startRecording() {
+        manager.accelerometerUpdateInterval = 0.1
+        
+        if manager.accelerometerAvailable {
+            self.manager.startAccelerometerUpdatesToQueue(NSOperationQueue()) {
+                (data, error) in
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    let x = data!.acceleration.x
+                    let y = data!.acceleration.y
+                    let z = data!.acceleration.z
+                    
+                    // start magic
+                    let sum = sqrt((x * x) + (y * y) + (z * z))
+                    
+                    if sum > 1.2 {
+                        self.counter++
+                        print("step \(self.counter)")
+                    }
+                    // end magic
+                }
+            }
+        }
+    }
+    
+}
