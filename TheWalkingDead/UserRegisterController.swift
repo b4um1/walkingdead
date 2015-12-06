d2d3f5589549d17a435e65c81ad32a55bfa22e72
Mario Baumgartner <mario.baumgartner92@gmx.at>
2015-12-06 17:23:48 +0100

    adaptions in registration view  - uiupdate  - added uielements

--- TheWalkingDead/UserRegisterController.swift
+++ TheWalkingDead/UserRegisterController.swift
@@ -11,7 +11,7 @@ import UIKit
 class UserRegisterController: UIViewController {
 
     @IBOutlet weak var button_createUser: UIButton!
-    @IBOutlet weak var uiview_createuser: UIView!
+    @IBOutlet weak var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +19,17 @@ class UserRegisterController: UIViewController {
     }
     
     func setUpUI(){
-        //set corners to panel
-        uiview_createuser.layer.cornerRadius=15
+        //hide navigation bar
+        navigationController?.navigationBarHidden = true
         
         //add border 
         button_createUser.layer.cornerRadius = button_createUser.frame.size.width/2-1
         button_createUser.layer.borderWidth = 2
         button_createUser.layer.borderColor = UIColor(red:255.0/255.0, green:255.0/255.0, blue:255.0/255.0, alpha: 1.0).CGColor
+        
+        //datepicker color
+        datePicker.setValue(UIColor.whiteColor(), forKeyPath: "textColor")
+        
     }
 }
 
