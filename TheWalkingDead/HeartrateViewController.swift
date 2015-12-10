d51a80700b7b2729ea5d384b5c91ccd45db4fdea
Mario Baumgartner <mario.baumgartner92@gmx.at>
2015-12-10 13:37:30 +0100

    GUI and animation of Heartratemonitor

--- TheWalkingDead/HeartrateViewController.swift
+++ TheWalkingDead/HeartrateViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class HeartrateViewController: UIViewController {
 
+    @IBOutlet weak var panel_labels: UIView!
+    @IBOutlet weak var l_currentHeartBeat: UILabel!
+    @IBOutlet weak var img_heart: UIImageView!
+    
+    @IBOutlet weak var l_avgbeats: UILabel!
+    @IBOutlet weak var l_maxbeat: UILabel!
+    
+    var beats = [Int]()
+    
+    var currentbeat = 86
+    let shrinkFactor = CGFloat(2.0 / 3)
+    var expandFactor: CGFloat {
+        return 1.0 / shrinkFactor
+    }
+    var duration: Double {
+        return 60.0 / Double(currentbeat)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        beat()
+        updateAverageBeats(calcAverageBeats())
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +43,56 @@ class HeartrateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupUI(){
+        panel_labels.layer.cornerRadius = 20
     }
-    */
+    
+    func calcAverageBeats() -> Int{
+        beats.append(80)
+        beats.append(80)
+        beats.append(80)
+        
+        var totalsum:Int = 0
+        
+        for beat in beats {
+            totalsum += beat
+        }
+        
+        return totalsum/beats.count;
+    }
+    
+    func updateAverageBeats(beat: Int){
+        self.l_avgbeats.text = NSString(format: "%d BPM", beat) as String
+    }
+    
+    
+    func beat() {
+        // 1
+        UIView.animateWithDuration(duration / 2,
+            delay: 0.0,
+            options: .CurveEaseInOut,
+            animations: {
+                // 2
+                self.img_heart.transform = CGAffineTransformScale(
+                    self.img_heart.transform, self.shrinkFactor, self.shrinkFactor)
+            },
+            completion: { _ in
+                // 3
+                UIView.animateWithDuration(self.duration / 2,
+                    delay: 0.0,
+                    options: .CurveEaseInOut,
+                    animations: {
+                        // 4
+                        self.img_heart.transform = CGAffineTransformScale(
+                            self.img_heart.transform, self.expandFactor, self.expandFactor)
+                    },
+                    completion: { _ in
+                        // 5
+                        self.beat()
+                    }
+                )
+            }
+        )
+    }
 
 }
